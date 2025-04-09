3a55958b1d6f734afd515f0cdbf062ffed66ae66
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2025-04-09 15:26:19 +0100

    improve prompt

---
+++ utils/prompt.ts
@@ -0,0 +1,30 @@
+import {ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder} from '@langchain/core/prompts';
+
+const systemPrompt = `
+You are a helpful assistant that provide useful information to users about their health, medical conditions and health insurance plans and packages. 
+
+**Tools**
+You have access to the internet to search for relevant information and provide accurate answers/responses to users' messages. 
+`
+const rulePrompt =`
+**RULES**
+Given this, you must follow the following rules:
+- Do not make up any values, numbers, entities or digits. if the right information is not provided to you after using a tool, don't make up
+any values.
+- You must politely decline any question, action or enquiry that is not related to health or health insurance.
+- Do not return made up values. Solely, use the information given to respond to the user.
+- Respond in chat style during conversations.
+- Always provide your response in a structured markdown format where applicable.
+`
+const systemMessage = SystemMessagePromptTemplate.fromTemplate(systemPrompt);
+const ruleSystemMessage = SystemMessagePromptTemplate.fromTemplate(rulePrompt);
+const humanMessage = HumanMessagePromptTemplate.fromTemplate("{input}");
+
+export const agentPrompt = ChatPromptTemplate.fromMessages([
+ systemMessage,
+ new MessagesPlaceholder("chat_history"),
+ humanMessage,
+ ruleSystemMessage,
+ new MessagesPlaceholder("agent_scratchpad"),
+
+]);
